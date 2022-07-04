6f1a934e3cc1414be8b45eefb12d440bb5244ad7
Deepak Choudhary <97680719+Dgr81996@users.noreply.github.com>
2022-07-05 05:24:18 +0530

    Add files via upload

---
+++ RCT-101.E2/src/App.js
@@ -0,0 +1,36 @@
+
+const container={ 
+
+
+  marginBottom: '50px',
+  marginLeft: '5%',
+  marginright:'5%',
+  
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+  function App() {
+    return (
+      <div>
+       
+       <button >Start</button> 
+       <button >Pause</button> 
+       <button >Skip by 30 sect</button> 
+      
+                 
+                      
+                  
+                 
+      </div>
+    );
+  }
+  
+  export default App;
+
+
